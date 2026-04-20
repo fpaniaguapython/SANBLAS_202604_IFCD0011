b82802ca36c5058a04909cc145b9a96dcc112be8
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2026-04-20 13:59:02 +0200

    Sesión 08

---
+++ ejercicios/25_tratamiento_peliculas.py
@@ -0,0 +1,11 @@
+diccionario_registros = dict()
+with open('peliculas.txt', mode='rt', encoding='utf-8') as fichero:
+    for registro in fichero:
+        registro = registro.replace('\n','') # Eliminamos los saltos de línea
+        registro = registro.strip() # Elíminamos los espacios en blanco de los extremos
+        lista_campos = registro.split(',') # Obtenemos una lista con los campos del registro
+        print(lista_campos)
+        diccionario_registros[(lista_campos[1][:-7]).upper()]=lista_campos
+
+pelicula = input('DIME QUÉ PELÍCULA QUIERES:')
+print(diccionario_registros[pelicula.upper()])
